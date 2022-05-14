5525e5db9f9568e24000f8318ce57748b92bd644
And4Web <anands2807@gmail.com>
2022-05-14 22:18:39 +0530

    homepage body created

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import SearchComponent from "../searchComponent/SearchComponent";
 import "./body.css";
+import googlelogo from "../../images/googleHomepage/googlelogo.jpg";
 
 export default function body() {
-  const googleLogo =
-    "https://cdn.vox-cdn.com/thumbor/p01ezbiuDHgRFQ-htBCd7QxaYxo=/0x105:2012x1237/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg";
   return (
     <div className="body">
-      <img src={googleLogo} alt="googleLogo" />
+      <img src={googlelogo} alt="googleLogo" />
       <SearchComponent />
     </div>
   );
--- src/components/searchComponent/SearchComponent.jsx
+++ src/components/searchComponent/SearchComponent.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./searchComponent.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button } from "@mui/material";
 
 export default function SearchComponent() {
-  return <div>SearchComponent</div>;
+  return (
+    <>
+      <form className="search">
+        <div className="search-input">
+          <SearchIcon className="searchIconCustom" />
+          <input />
+        </div>
+        <div className="search-buttons">
+          <Button className="buttonCustom" type="submit" varient="outline">
+            Google search
+          </Button>
+          <Button className="buttonCustom" varient="outline">
+            I'm feeling Lucky
+          </Button>
+        </div>
+      </form>
+    </>
+  );
 }
