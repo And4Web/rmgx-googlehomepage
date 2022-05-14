7523c861951b18a8e46f781b860f5825357ba8ca
And4Web <anands2807@gmail.com>
2022-05-14 16:33:35 +0530

    project deployed using gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Google HomePage</h1>
+      <h1>Google HomePage is deployed on github using gh-pages.</h1>
     </div>
   );
 }
