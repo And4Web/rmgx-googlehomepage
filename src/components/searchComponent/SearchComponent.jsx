aad73910af5ccc6645dac311de05bc0f0ab86c58
And4Web <anands2807@gmail.com>
2022-05-19 07:13:03 +0530

    adding a central store for state management using context API

--- src/components/searchComponent/SearchComponent.jsx
+++ src/components/searchComponent/SearchComponent.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./searchComponent.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { Button } from "@mui/material";
 
 export default function SearchComponent(props) {
   const { hideButtons } = props;
+
+  const [state, setState] = useState();
+  const nav = useNavigate();
+  console.log(state);
+
+  const onClick = (e) => {
+    e.preventDefault();
+    nav("/search");
+  };
+
   return (
     <>
       <form className="search">
         <div className="search-input">
           <SearchIcon className="searchIconCustom" />
-          <input />
+          <input value={state} onChange={(e) => setState(e.target.value)} />
         </div>
         {!hideButtons && (
           <div className="search-buttons">
-            <Button className="buttonCustom" type="submit" varient="outline">
+            <Button
+              onClick={onClick}
+              className="buttonCustom"
+              type="submit"
+              varient="outline"
+            >
               Google search
             </Button>
             <Button className="buttonCustom" varient="outline">
